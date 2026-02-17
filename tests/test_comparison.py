d95433fdb56f83246f19a78361841f023408be4b
Vincent Delecroix <vincent.delecroix@u-bordeaux.fr>
2026-02-17 09:59:12 +0100

    finer comparison tests

--- tests/test_comparison.py
+++ tests/test_comparison.py
@@ -17,53 +17,73 @@
 # along with veerer. If not, see <https://www.gnu.org/licenses/>.
 ######################################################################
 import pytest
-from veerer import VeeringTriangulation, VeeringTriangulationLinearFamily
+from veerer import VeeringTriangulation, StrebelGraph, VeeringTriangulationLinearFamily, StrebelGraphLinearFamily
 
-def test_comparison():
+def _check_comparison_consistency(a, b):
+    lt = a < b
+    le = a <= b
+    eq = a == b
+    ne = a != b
+    gt = a > b
+    ge = a >= b
+
+    assert lt == (b > a)
+    assert le == (b >= a)
+    assert eq == (b == a)
+    assert ne == (b != a)
+    assert gt == (b < a)
+    assert ge == (b <= a)
+
+    assert lt + eq + gt == 1
+    assert eq + ne == 1
+    assert lt + ge == 1
+    assert gt + le == 1
+    assert lt + gt == ne
+    assert le == lt + eq
+    assert ge == gt + eq
+
+def _check_comparison_sample_consistency(sample):
+    for a in sample:
+        for b in sample:
+            _check_comparison_consistency(a, b)
+
+
+def test_veering_triangulation_comparison():
     vt0 = VeeringTriangulation("(0,1,2)(~0,~1,3)(~2,4,5)(~3,~4,6)(~5,7,8)(~6,~7,~8)", "RRBBRRRRB")
     vt1 = VeeringTriangulation("(0,1,2)(~0,~1,3)(~2,4,5)(~3,~4,6)(~5,7,8)(~6,~7,~8)", "BRBBRBBRR")
     vt2 = VeeringTriangulation("(0:1)(~0:1,1:1,2:1)(~1:1,~2:1,3:1)(~3:1)", "RRBR")
     vt3 = VeeringTriangulation("(0:1)(~0:1,1:1,2:1)(~1:1,~2:1,3:1)(~3:1)", "BRRB")
 
     sample = [vt0, vt1, vt2, vt3]
+    fsample = [vt.as_linear_family() for vt in sample]
     for i in range(4):
         for j in range(4):
-            assert (sample[i] == sample[j]) == (i == j)
-            assert (sample[i] != sample[j]) == (i != j)
+            assert (sample[i] == sample[j]) == (sample[i] == fsample[j]) == (fsample[i] == sample[j]) == (fsample[i] == fsample[j]) == (i == j)
+            assert (sample[i] != sample[j]) == (sample[i] != fsample[j]) == (fsample[i] != sample[j]) == (fsample[i] != fsample[j]) == (i != j)
 
-    f0 = vt0.as_linear_family()
-    f1 = vt1.as_linear_family()
-    sample = [f0, f1]
-    for i in range(2):
-        for j in range(2):
-            assert (sample[i] == sample[j]) == (i == j)
-            assert (sample[i] != sample[j]) == (i != j)
 
-    assert vt0 == f0 and f0 == vt0
-    assert vt1 == f1 and f1 == vt1
-    assert f0 != vt1 and vt1 != f0
-    assert vt0 != f1 and f1 != vt0
+    f4 = VeeringTriangulationLinearFamily("(0:1)(~0:1,1:1,2:1)(~1:1,~2:1,3:1)(~3:1)", "RRBR", [(1, 0, 0, 1), (0, 1, 0, 0), (0, 0, 1, 0)])
+    f5 = VeeringTriangulationLinearFamily("(0:1)(~0:1,1:1,2:1)(~1:1,~2:1,3:1)(~3:1)", "BRRB", [(1, 0, 0, 1), (0, 1, 0, 0), (0, 0, 1, 0)])
 
-    f2 = VeeringTriangulationLinearFamily("(0:1)(~0:1,1:1,2:1)(~1:1,~2:1,3:1)(~3:1)", "RRBR", [(1, 0, 0, 1), (0, 1, 0, 0), (0, 0, 1, 0)])
-    f3 = VeeringTriangulationLinearFamily("(0:1)(~0:1,1:1,2:1)(~1:1,~2:1,3:1)(~3:1)", "BRRB", [(1, 0, 0, 1), (0, 1, 0, 0), (0, 0, 1, 0)])
+    _check_comparison_sample_consistency(sample)
+    _check_comparison_sample_consistency(fsample + [f4, f5])
 
-    assert vt2 != f2 and f2 != vt2
-    assert vt3 != f3 and f3 != vt3
-    assert vt2.as_linear_family() != f2
 
-    sample = [vt0, vt1, vt2, vt3]
-    for a in sample:
-        for b in sample:
-            assert (a < b) != (a >= b)
-            assert (a > b) != (a <= b)
-            assert (a < b) + (a == b) + (a > b) == 1
+def test_strebel_graph_comparison():
+    sg0 = StrebelGraph("(0,~0:1)")
+    sg1 = StrebelGraph("(0,~0:2)")
+    sg2 = StrebelGraph("(0:1,1:0,~1:1,~0:0)")
 
-    sample = [f0, f1, f2, f3, vt2.as_linear_family(), vt3.as_linear_family()]
-    for a in sample:
-        for b in sample:
-            assert (a < b) != (a >= b)
-            assert (a > b) != (a <= b)
-            assert (a < b) + (a == b) + (a > b) == 1
+    sample = [sg0, sg1, sg2]
+    fsample = [sg.as_linear_family() for sg in sample]
+    for i in range(3):
+        for j in range(3):
+            assert (sample[i] == sample[j]) == (sample[i] == fsample[j]) == (fsample[i] == sample[j]) == (fsample[i] == fsample[j]) == (i == j)
+            assert (sample[i] != sample[j]) == (sample[i] != fsample[j]) == (fsample[i] != sample[j]) == (fsample[i] != fsample[j]) == (i != j)
+
+    _check_comparison_sample_consistency(sample)
+    _check_comparison_sample_consistency(fsample)
+
 
 if __name__ == '__main__':
     import sys
